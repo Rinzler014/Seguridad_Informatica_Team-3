b25057c03257d2b957291d18f7a1066cb1423e5b
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2022-09-07 21:24:08 -0500

    Creacion de segundo formulario

---
+++ app/src/main/java/com/example/startform/AccountCreation.kt
@@ -0,0 +1,13 @@
+package com.example.startform
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountCreation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.account_layout)
+    }
+
+}
