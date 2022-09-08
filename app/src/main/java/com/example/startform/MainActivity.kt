656c66092096bdfd352aaaf27b7d874460d5ab0c
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2022-09-08 15:48:06 -0500

    Añadidas Validaciones en campos

--- app/src/main/java/com/example/startform/MainActivity.kt
+++ app/src/main/java/com/example/startform/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.startform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toolbar
+import androidx.core.widget.addTextChangedListener
+import java.io.Serializable
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,46 +35,90 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val userPersonalName = findViewById<EditText>(R.id.field_name_edit)
+        val userLastName = findViewById<EditText>(R.id.field_last_name_edit)
+        val userMotherLastName = findViewById<EditText>(R.id.field_mother_last_name_edit)
+
         val dayField = findViewById<EditText>(R.id.field_birth_date_day_edit)
         val monthField = findViewById<EditText>(R.id.field_birth_date_month_edit)
         val yearField = findViewById<EditText>(R.id.field_birth_date_year_edit)
 
-        dayField.setOnFocusChangeListener { _, _ ->
+        val continueButton = findViewById<Button>(R.id.button_continue)
+        continueButton.isEnabled = false
+
+        var aux1 = false
+        var aux2 = false
+        var aux3 = false
+
+
+        dayField.addTextChangedListener {
 
             val day = dayField.text.toString()
 
             if (day.isNotEmpty()){
                 if(!checkValidDates(day, 1)) {
-                    dayField.error = "Dia Invalido"
+                    dayField.error = getString(R.string.day_error)
+                    aux1 = false
+                } else {
+                    aux1 = true
                 }
+            } else {
+                aux1 = false
             }
+
+            continueButton.isEnabled = aux1 && aux2 && aux3
 
         }
 
-        monthField.setOnFocusChangeListener {_, _ ->
+        monthField.addTextChangedListener {
 
             val month = monthField.text.toString()
 
             if (month.isNotEmpty()) {
                 if (!checkValidDates(month, 2)) {
-                    monthField.error = "Mes Invalido"
+                    monthField.error = getString(R.string.month_error)
+                    aux2 = false
+                } else {
+                    aux2 = true
                 }
+            } else {
+                aux2 = false
             }
+
+            continueButton.isEnabled = aux1 && aux2 && aux3
 
         }
 
-        yearField.setOnFocusChangeListener { _, _ ->
+        yearField.addTextChangedListener {
 
             val year = yearField.text.toString()
 
             if(year.isNotEmpty()) {
                 if (!checkValidDates(year, 3)) {
-                    yearField.error = "Año Invalido"
+                    yearField.error = getString(R.string.year_error)
+                    aux3 = false
+                } else {
+                    aux3 = true
                 }
+            } else {
+                aux3 = false
             }
+
+            continueButton.isEnabled = aux1 && aux2 && aux3
+
 
         }
 
+        continueButton.setOnClickListener {
+
+            val intent = Intent(this, AccountCreation::class.java)
+            val userPersonalInformation : ArrayList<String> = arrayListOf(userPersonalName.text.toString(), userLastName.text.toString(), userMotherLastName.text.toString())
+
+            intent.putExtra(AccountCreation.PERSONAL_INFORMATION, userPersonalInformation)
+
+            startActivity(intent)
+
+        }
 
     }
 }
